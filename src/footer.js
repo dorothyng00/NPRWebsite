18f3448288dbbe92bf81ceb26e3212ffa12f1b9d
Dorothy <dthuc.nguyen.120@gmail.com>
2021-08-13 23:08:36 -0400

    asf
    asf

--- src/footer.js
+++ src/footer.js
@@ -1,10 +1,10 @@
 const footer = document.getElementById('footer');
 footer.innerHTML = `
 <div class="w-full" style="background-color:var(--darkblue);">
-    <div class="grid grid-cols-3 gap-10 container mx-auto">
-        <div style="padding-left:5%; padding-right:5%">
+    <div class="container mx-auto">
+        <div class="grid grid-cols-3 gap-10" style="padding: 5%;">
             <div>
-                <img src="/assets/logo-white.png" />
+                <img src="/assets/logo-white.png" style="object-fit: contain;" />
             </div>
             <div>
                 <h6>Head Office</h6>
