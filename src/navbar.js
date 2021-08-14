0950ab6662c01b82ef8ad5e8be17ca7d9d22b9ff
Justin <justin@redngen.com>
2021-08-13 23:02:43 -0400

    asf
    asf

--- src/navbar.js
+++ src/navbar.js
@@ -1,12 +1,13 @@
 const navbar = document.getElementById('nav-bar');
 navbar.innerHTML = `
-<div class="nav-wrapper md:container md:mx-auto" :style="[showMenu ? {'background-color':'#FFF'} : {'background-color':'transparent'}]">
+<div class="nav-wrapper md:container md:mx-auto" style="background-color: transparent;">
   <nav class="navbar container-padding">
-    <img @click="navigateHome()" src="/assets/logo-white.png" alt="Company Logo" style="cursor: pointer;">
-    <div class="menu-toggle" :class="showMenu ? 'is-active' : ''" id="mobile-menu" @click="toggleMenu()">
-      <span class="bar"></span>
-      <span class="bar"></span>
-      <span class="bar"></span>
+    <img v-if="showMenu" @click="navigateHome()" src="/assets/logo-black.png" alt="Company Logo" style="cursor: pointer; z-index: 100;">
+    <img v-else @click="navigateHome()" src="/assets/logo-white.png" alt="Company Logo" style="cursor: pointer; z-index: 100;">
+    <div class="menu-toggle" :class="showMenu ? 'is-active' : ''" id="mobile-menu" @click="toggleMenu()" style="z-index: 100;">
+      <span class="bar" :style="[showMenu ? {'background-color':'#000'} : {'':''}]"></span>
+      <span class="bar" :style="[showMenu ? {'background-color':'#000'} : {'':''}]"></span>
+      <span class="bar" :style="[showMenu ? {'background-color':'#000'} : {'':''}]"></span>
     </div>
     <ul class="sm-font-size bodyFont nav no-search" :class="showMenu ? 'mobile-nav' : ''" style="line-height: 1.2;">
       <li class="nav-item"><a href="/src/">Home</a></li>
