53749a738bebb5cbfcca579a131fd92e340cd52d
Justin <justin@redngen.com>
2021-08-13 23:06:08 -0400

    asf
    asf

--- src/footer.js
+++ src/footer.js
@@ -1,15 +1,17 @@
 const footer = document.getElementById('footer');
 footer.innerHTML = `
-<div style="background-color:var(--darkblue)">
-    <div class="grid grid-cols-3 gap-10 container mx-auto" style="padding-left:5%; padding-right:5%">
-        <div>
-            <img src="/assets/logo-white.png" />
-        </div>
-        <div>
-            <h6>Head Office</h6>
-        </div>
-        <div>
-            <div>navigation</div>
+<div class="w-full" style="background-color:var(--darkblue);">
+    <div class="grid grid-cols-3 gap-10 container mx-auto">
+        <div style="padding-left:5%; padding-right:5%">
+            <div>
+                <img src="/assets/logo-white.png" />
+            </div>
+            <div>
+                <h6>Head Office</h6>
+            </div>
+            <div>
+                <div>navigation</div>
+            </div>
         </div>
     </div>
 </div>
