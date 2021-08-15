b971ce4aeaf3f086526d492aeb321c50c23fccdf
Justin <justin@redngen.com>
2021-08-15 17:04:10 -0400

    asf
    asf

--- src/navbar.js
+++ src/navbar.js
@@ -2,14 +2,21 @@ const navbar = document.getElementById('nav-bar');
 navbar.innerHTML = `
 <div class="nav-wrapper md:container md:mx-auto" style="background-color: transparent;">
   <nav class="navbar container-padding">
-    <img v-if="showMenu" @click="navigateHome()" src="/assets/logo-black.png" alt="Company Logo" style="cursor: pointer; z-index: 100;">
-    <img v-else @click="navigateHome()" src="/assets/logo-white.png" alt="Company Logo" style="cursor: pointer; z-index: 100;">
-    <div class="menu-toggle" :class="showMenu ? 'is-active' : ''" id="mobile-menu" @click="toggleMenu()" style="z-index: 100;">
+    <img v-if="!showMenu" @click="navigateHome()" src="/assets/logo-white.png" alt="Company Logo" style="cursor: pointer;">
+    <div class="menu-toggle" :class="showMenu ? 'is-active' : ''" id="mobile-menu" @click="toggleMenu()">
       <span class="bar" :style="[showMenu ? {'background-color':'#000'} : {'':''}]"></span>
       <span class="bar" :style="[showMenu ? {'background-color':'#000'} : {'':''}]"></span>
       <span class="bar" :style="[showMenu ? {'background-color':'#000'} : {'':''}]"></span>
     </div>
     <ul class="sm-font-size bodyFont nav no-search" :class="showMenu ? 'mobile-nav' : ''" style="line-height: 1.2;">
+      <div v-if="showMenu" class="navbar container-padding">
+        <img @click="navigateHome()" src="/assets/logo-black.png" alt="Company Logo" style="cursor: pointer;">
+        <div class="menu-toggle" :class="showMenu ? 'is-active' : ''" id="mobile-menu" @click="toggleMenu()">
+          <span class="bar" :style="[showMenu ? {'background-color':'#000'} : {'':''}]"></span>
+          <span class="bar" :style="[showMenu ? {'background-color':'#000'} : {'':''}]"></span>
+          <span class="bar" :style="[showMenu ? {'background-color':'#000'} : {'':''}]"></span>
+        </div>
+      </div>
       <li class="nav-item"><a href="/src/">Home</a></li>
       <li class="nav-item"><a href="/src/property-management.html">Property Management</a></li>
       <li class="nav-item"><a href="/src/tenant-placement.html">Tenant Placement</a></li>
