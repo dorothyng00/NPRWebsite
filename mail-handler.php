c6393b5ba32b11de7031a384415936bbb77a48ec
Justin <justin.yang8499@gmail.com>
2021-08-19 11:55:58 -0400

    asf
    asf

--- mail-handler.php
+++ mail-handler.php
@@ -1,20 +1,35 @@
 <?php 
-if(isset($_POST['submit'])){
-    $to = "justin.yang8499@gmail.com"; // this is your Email address
-    $from = $_POST['email']; // this is the sender's Email address
-    $firstName = $_POST['firstName'];
-    $lastName = $_POST['lastName'];
+// if(isset($_POST['submit'])){
+//     $to = "justin.yang8499@gmail.com";
+//     $from = $_POST['email'];
+//     $firstName = $_POST['firstName'];
+//     $lastName = $_POST['lastName'];
+//     $subject = "Form submission";
+//     $subject2 = "Copy of your form submission";
+//     $message = $firstName . " " . $lastName . " wrote the following:" . "\n\n" . $_POST['message'];
+//     $message2 = "Here is a copy of your message " . $firstName . "\n\n" . $_POST['message'];
+
+//     $headers = "From:" . $from;
+//     $headers2 = "From:" . $to;
+//     mail($to,$subject,$message,$headers);
+//     mail($from,$subject2,$message2,$headers2);
+//     echo "Mail Sent. Thank you " . $firstName . ", we will contact you shortly.";
+//     }
+
+if(isset($_GET['submit'])){
+    $to = "justin.yang8499@gmail.com";
+    $from = $_GET['email'];
+    $firstName = $_GET['firstName'];
+    $lastName = $_GET['lastName'];
     $subject = "Form submission";
     $subject2 = "Copy of your form submission";
-    $message = $firstName . " " . $lastName . " wrote the following:" . "\n\n" . $_POST['message'];
-    $message2 = "Here is a copy of your message " . $firstName . "\n\n" . $_POST['message'];
+    $message = $firstName . " " . $lastName . " wrote the following:" . "\n\n" . $_GET['message'];
+    $message2 = "Here is a copy of your message " . $firstName . "\n\n" . $_GET['message'];
 
     $headers = "From:" . $from;
     $headers2 = "From:" . $to;
     mail($to,$subject,$message,$headers);
-    mail($from,$subject2,$message2,$headers2); // sends a copy of the message to the sender
+    mail($from,$subject2,$message2,$headers2);
     echo "Mail Sent. Thank you " . $firstName . ", we will contact you shortly.";
-    // You can also use header('Location: thank_you.php'); to redirect to another page.
-    // You cannot use header and echo together. It's one or the other.
     }
 ?>
