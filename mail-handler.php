cb0e8f1144bca9687a0b4f6247498c8b5556be76
Justin <justin.yang8499@gmail.com>
2021-08-19 12:01:10 -0400

    asf
    asf

--- mail-handler.php
+++ mail-handler.php
@@ -1,30 +1,13 @@
 <?php 
-// if(isset($_POST['submit'])){
-//     $to = "justin.yang8499@gmail.com";
-//     $from = $_POST['email'];
-//     $firstName = $_POST['firstName'];
-//     $lastName = $_POST['lastName'];
-//     $subject = "Form submission";
-//     $subject2 = "Copy of your form submission";
-//     $message = $firstName . " " . $lastName . " wrote the following:" . "\n\n" . $_POST['message'];
-//     $message2 = "Here is a copy of your message " . $firstName . "\n\n" . $_POST['message'];
-
-//     $headers = "From:" . $from;
-//     $headers2 = "From:" . $to;
-//     mail($to,$subject,$message,$headers);
-//     mail($from,$subject2,$message2,$headers2);
-//     echo "Mail Sent. Thank you " . $firstName . ", we will contact you shortly.";
-//     }
-
-if(isset($_GET['submit'])){
+if(isset($_POST['submit'])){
     $to = "justin.yang8499@gmail.com";
-    $from = $_GET['email'];
-    $firstName = $_GET['firstName'];
-    $lastName = $_GET['lastName'];
+    $from = $_POST['email'];
+    $firstName = $_POST['firstName'];
+    $lastName = $_POST['lastName'];
     $subject = "Form submission";
     $subject2 = "Copy of your form submission";
-    $message = $firstName . " " . $lastName . " wrote the following:" . "\n\n" . $_GET['message'];
-    $message2 = "Here is a copy of your message " . $firstName . "\n\n" . $_GET['message'];
+    $message = $firstName . " " . $lastName . " wrote the following:" . "\n\n" . $_POST['message'];
+    $message2 = "Here is a copy of your message " . $firstName . "\n\n" . $_POST['message'];
 
     $headers = "From:" . $from;
     $headers2 = "From:" . $to;
@@ -32,4 +15,21 @@ if(isset($_GET['submit'])){
     mail($from,$subject2,$message2,$headers2);
     echo "Mail Sent. Thank you " . $firstName . ", we will contact you shortly.";
     }
-?>
+
+// if(isset($_GET['submit'])){
+//     $to = "justin.yang8499@gmail.com";
+//     $from = $_GET['email'];
+//     $firstName = $_GET['firstName'];
+//     $lastName = $_GET['lastName'];
+//     $subject = "Form submission";
+//     $subject2 = "Copy of your form submission";
+//     $message = $firstName . " " . $lastName . " wrote the following:" . "\n\n" . $_GET['message'];
+//     $message2 = "Here is a copy of your message " . $firstName . "\n\n" . $_GET['message'];
+
+//     $headers = "From:" . $from;
+//     $headers2 = "From:" . $to;
+//     mail($to,$subject,$message,$headers);
+//     mail($from,$subject2,$message2,$headers2);
+//     echo "Mail Sent. Thank you " . $firstName . ", we will contact you shortly.";
+//     }
+// ?>
