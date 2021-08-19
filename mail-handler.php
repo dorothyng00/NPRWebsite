5e1cc76b64463354d3f2ca24ba8ea1897de49bc7
Justin <justin.yang8499@gmail.com>
2021-08-19 12:05:07 -0400

    asf
    asf

--- mail-handler.php
+++ mail-handler.php
@@ -15,21 +15,4 @@ if(isset($_POST['submit'])){
     mail($from,$subject2,$message2,$headers2);
     echo "Mail Sent. Thank you " . $firstName . ", we will contact you shortly.";
     }
-
-// if(isset($_GET['submit'])){
-//     $to = "justin.yang8499@gmail.com";
-//     $from = $_GET['email'];
-//     $firstName = $_GET['firstName'];
-//     $lastName = $_GET['lastName'];
-//     $subject = "Form submission";
-//     $subject2 = "Copy of your form submission";
-//     $message = $firstName . " " . $lastName . " wrote the following:" . "\n\n" . $_GET['message'];
-//     $message2 = "Here is a copy of your message " . $firstName . "\n\n" . $_GET['message'];
-
-//     $headers = "From:" . $from;
-//     $headers2 = "From:" . $to;
-//     mail($to,$subject,$message,$headers);
-//     mail($from,$subject2,$message2,$headers2);
-//     echo "Mail Sent. Thank you " . $firstName . ", we will contact you shortly.";
-//     }
-// ?>
+?>
