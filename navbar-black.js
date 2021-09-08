65cde0a330a93d7313e08fa4bf8c62f0951859e9
Justin <justin.yang8499@gmail.com>
2021-09-07 22:59:12 -0400

    asf
    asf

--- navbar-black.js
+++ navbar-black.js
@@ -9,13 +9,13 @@ navbar.innerHTML = `
       <span class="bar" style="background-color: #000;"></span>
     </div>
     <ul class="sm-font-size bodyFont nav no-search" :class="showMenu ? 'mobile-nav' : ''" style="line-height: 1.2;">
-      <li class="nav-item-black"><a href="/NPRWebsite" :style="[currentTab == 'home' ? {'color':'var(--darkorange)'} : {'':''}]">Home</a></li>
-      <li class="nav-item-black"><a href="/NPRWebsite/property-management" :style="[currentTab == 'property-management' ? {'color':'var(--darkorange)'} : {'':''}]">Property Management</a></li>
-      <li class="nav-item-black"><a href="/NPRWebsite/tenant-placement" :style="[currentTab == 'tenant-placement' ? {'color':'var(--darkorange)'} : {'':''}]">Tenant Placement</a></li>
-      <li class="nav-item-black"><a href="/NPRWebsite/tenant-rental-application" :style="[currentTab == 'tenant-rental-application' ? {'color':'var(--darkorange)'} : {'':''}]">Tenant Rental Application</a></li>
-      <li class="nav-item-black"><a href="/NPRWebsite/faq" :style="[currentTab == 'faq' ? {'color':'var(--darkorange)'} : {'':''}]">FAQ</a></li>
-      <li class="nav-item-black"><a href="/NPRWebsite/contact" :style="[currentTab == 'contact' ? {'color':'var(--darkorange)'} : {'':''}]">Contact Us</a></li>
-      <li class="nav-item-black"><a href="/NPRWebsite/properties" :style="[currentTab == 'properties' ? {'color':'var(--darkorange)'} : {'':''}]">Properties</a></li>
+      <li class="nav-item-black"><a href="/NPRWebsite" :style="[currentTab == 'home' ? {'color':'var(--darkblue)'} : {'':''}]">Home</a></li>
+      <li class="nav-item-black"><a href="/NPRWebsite/property-management" :style="[currentTab == 'property-management' ? {'color':'var(--darkblue)'} : {'':''}]">Property Management</a></li>
+      <li class="nav-item-black"><a href="/NPRWebsite/tenant-placement" :style="[currentTab == 'tenant-placement' ? {'color':'var(--darkblue)'} : {'':''}]">Tenant Placement</a></li>
+      <li class="nav-item-black"><a href="/NPRWebsite/tenant-rental-application" :style="[currentTab == 'tenant-rental-application' ? {'color':'var(--darkblue)'} : {'':''}]">Tenant Rental Application</a></li>
+      <li class="nav-item-black"><a href="/NPRWebsite/faq" :style="[currentTab == 'faq' ? {'color':'var(--darkblue)'} : {'':''}]">FAQ</a></li>
+      <li class="nav-item-black"><a href="/NPRWebsite/contact" :style="[currentTab == 'contact' ? {'color':'var(--darkblue)'} : {'':''}]">Contact Us</a></li>
+      <li class="nav-item-black"><a href="/NPRWebsite/properties" :style="[currentTab == 'properties' ? {'color':'var(--darkblue)'} : {'':''}]">Properties</a></li>
     </ul>
   </nav>
   </div>
